fffa0a8251d220ead84afe74528917fe800752b3
omarturki <omarturkich@gmail.com>
2021-01-03 20:11:00 +0100

    initialise project

---
+++ src/test/java/tekup/RestoApplicationTests.java
@@ -0,0 +1,13 @@
+package tekup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
